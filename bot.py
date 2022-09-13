fa5c58d004ae90fb9ebfde79c1b63cb69bd3e4c1
Садыков Роберт Радикович <rrsadykov_1@edu.hse.ru>
2022-09-13 03:44:29 +0300

    Initial commit

---
+++ bot.py
@@ -0,0 +1,49 @@
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+from aiogram.dispatcher.filters import Text
+
+from config import TOKEN
+
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+# @test6767test_bot
+
+# старт
+@dp.message_handler(commands="start")
+async def process_start_command(message: types.Message):
+    kb = [
+        [types.KeyboardButton(text="Книги")],
+        [types.KeyboardButton(text="Кино")],
+        [types.KeyboardButton(text="Трейдинг")],
+    ]
+    keyboard = types.ReplyKeyboardMarkup(keyboard=kb,
+                                         resize_keyboard=True,
+                                         input_field_placeholder="Выберите интересующий вариант из меню"
+                                         )
+    await message.answer("Что вам интересно?", reply_markup=keyboard)
+
+
+@dp.message(Text(text="Книги"))
+async def with_puree(message: types.Message):
+    await message.reply("Отличный выбор!")
+
+@dp.message(lambda message: message.text == "Без пюрешки")
+async def without_puree(message: types.Message):
+    await message.reply("Так невкусно!")
+
+# echo - заглушка
+
+@dp.message_handler()
+async def echo_message(msg: types.Message):
+    await bot.send_message(msg.from_user.id, msg.text)
+
+
+@dp.message_handler(commands=['help'])
+async def process_help_command(message: types.Message):
+    await message.reply("С помощью этого бота ты можешь получить много полезной информации и записаться на обучение")
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
