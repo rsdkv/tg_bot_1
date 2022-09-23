b71d174b0456d617578ac04e723a5650d862563e
Robert <rrsadykov_1@edu.hse.ru>
2022-09-23 10:30:54 +0300

    upd markup

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ from aiogram.dispatcher.filters import Text
 import random
 from config import TOKEN
 
+import markup as nav
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
@@ -14,25 +15,29 @@ dp = Dispatcher(bot)
 
 # старт
 @dp.message_handler(commands="start")
-async def process_start_command(message: types.Message):
-    kb = [
-        [types.KeyboardButton(text="Книги")],
-        [types.KeyboardButton(text="Кино")],
-        [types.KeyboardButton(text="Трейдинг")],
-    ]
-    keyboard = types.ReplyKeyboardMarkup(keyboard=kb,
-                                         resize_keyboard=True,
-                                         input_field_placeholder="Выберите интересующий вариант из меню"
-                                         )
-    await message.answer("Что вам интересно?", reply_markup=keyboard)
+async def command_start(message: types.Message):
+    await bot.send_message(message.from_user.   id, 'Привет{0.first_name}'.format(message.from_user), reply_markup=nav.mainMenu)
 
 
-@dp.message_handler(text="Книги")
+@dp.message_handler()
 async def with_puree(message: types.Message):
-    with open("books.txt", "r") as books:
-        lines = random.sample(list(books), 10)
-        await message.reply(*map(str.strip, lines))
+    if message.text == 'Учиться':
+        await bot.send_message(message.from_user.id, 'Здесь вы можете найти... \n '
+                                                     'весь этот текст нужно будет вынести '
+                                                     'в отдельный файл и мне дать',
+                               reply_markup=nav.eduMenu)
 
+    elif message.text == 'Главное меню':
+        await bot.send_message(message.from_user.id, 'возращение в главное меню', reply_markup=nav.mainMenu)
+    elif message.text == 'Стать лучше':
+        await bot.send_message(message.from_user.id, 'тут я не понял куда ведёт, можно ещё раз написать?', reply_markup=nav.improveMenu)
+    elif message.text == 'Книги':
+        await bot.send_message(message.from_user.id, 'тут будет список книг,\n  '
+                                                     'это тоже нужно дать текстом мне',
+                               reply_markup=nav.improveMenu)
+
+    else:
+        await message.reply('Неизветсная команда')
 
 @dp.message_handler(lambda message: message.text == "1-->111")
 async def without_puree(message: types.Message):
--- markup.py
+++ markup.py
@@ -1,11 +1,17 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
+btnMain = KeyboardButton('Главное меню')
 # main menu
-btnRandom = KeyboardButton('rand digit')
-btnOther = KeyboardButton('другое')
-mainMenu = ReplyKeyboardMarkup(resize_keyboard=True).add(btnRandom, btnOther)
+btnEdu = KeyboardButton('Учиться')
+btnImprove = KeyboardButton('Стать лучше')
+btnFun = KeyboardButton('Развлечься')
+mainMenu = ReplyKeyboardMarkup(resize_keyboard=True).add(btnEdu, btnImprove, btnFun)
 
 
-#other menu
-btnInfo= KeyboardButton('info')
-btnLink= KeyboardButton('link')
+#edu menu - из кнопок: вызов главного меню  --   Обучение
+eduMenu = ReplyKeyboardMarkup(resize_keyboard= True).add(btnMain)
+
+# меню для   --  Стать лучше
+btnLibrary = KeyboardButton('Книги')
+btnForTrader = KeyboardButton('Трейдеру')
+improveMenu = ReplyKeyboardMarkup(resize_keyboard= True).add(btnLibrary, btnForTrader, btnMain)
