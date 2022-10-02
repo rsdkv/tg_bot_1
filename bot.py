938656afe31cf1d1d1b408bd2130f306c4e46cb8
Садыков Роберт Радикович <rrsadykov_1@edu.hse.ru>
2022-10-02 17:40:49 +0300

    finish upd

--- bot.py
+++ bot.py
@@ -1,47 +1,104 @@
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
-from aiogram.dispatcher.filters import Text
 
-import random
+
 from config import TOKEN
 
 import markup as nav
+
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
-
 # @test6767test_bot
 
+# обработчик списка фильмов
+with open('text_films.txt', encoding='utf8') as f:
+    lines = f.readlines()
+films_text = ''
+for item in lines:
+    films_text += item
+
+# обработчик списка книг
+with open('text_books.txt', encoding='utf8') as f:
+    lines = f.readlines()
+books_text = ''
+for item in lines:
+    books_text += item
+
+# обработчик текста обучения
+with open('text_edu.txt', encoding='utf8') as f:
+    lines = f.readlines()
+edu_text = ''
+for item in lines:
+    edu_text += item
+
+# обработчик текста музыки
+with open('text_musics.txt', encoding='utf8') as f:
+    lines = f.readlines()
+music_text = ''
+for item in lines:
+    music_text += item
+
+# обработчик текста ссылок
+with open('text_links.txt', encoding='utf8') as f:
+    lines = f.readlines()
+links_text = ''
+for item in lines:
+    links_text += item
+
+# обработчик текста арбитража
+with open('text_arbitrage.txt', encoding='utf8') as f:
+    text_arbitrage = f.read()
+
+
 # старт
-@dp.message_handler(commands="start")
+@dp.message_handler(commands=['start', 'начать', 'старт'])
 async def command_start(message: types.Message):
-    await bot.send_message(message.from_user.   id, 'Привет{0.first_name}'.format(message.from_user), reply_markup=nav.mainMenu)
+    await bot.send_message(message.from_user.id, 'Привет {0.first_name} '
+                                                 'Я Гендиректор Виктори, бот, созданный '
+                                                 'для помощи вам. Что вы хотели бы?'.format(message.from_user),
+                           reply_markup=nav.mainMenu)
 
 
+# меню
 @dp.message_handler()
 async def with_puree(message: types.Message):
-    if message.text == 'Учиться':
-        await bot.send_message(message.from_user.id, 'Здесь вы можете найти... \n '
-                                                     'весь этот текст нужно будет вынести '
-                                                     'в отдельный файл и мне дать',
+    # главное меню
+    if message.text == 'Главное меню':
+        await bot.send_message(message.from_user.id, 'Главное меню', reply_markup=nav.mainMenu)
+    elif message.text == 'Учиться':
+        await bot.send_message(message.from_user.id,
+                               'Здесь вы можете найти всё, что вам может понадобиться для обучения: \n' + edu_text,
                                reply_markup=nav.eduMenu)
-
-    elif message.text == 'Главное меню':
-        await bot.send_message(message.from_user.id, 'возращение в главное меню', reply_markup=nav.mainMenu)
     elif message.text == 'Стать лучше':
-        await bot.send_message(message.from_user.id, 'тут я не понял куда ведёт, можно ещё раз написать?', reply_markup=nav.improveMenu)
+        await bot.send_message(message.from_user.id,
+                               'Здесь вы можете найти все, что вам может понадобиться. Выберите интересующую вас тему',
+                               reply_markup=nav.traderMenu)
+    elif message.text == 'Развлечься':
+        await bot.send_message(message.from_user.id, 'Вы можете не только развлечься, но и прокачать свой мозг',
+                               reply_markup=nav.libraryMenu)
+        # библиотека подменю
+    elif message.text == 'Фильмы':
+        await bot.send_message(message.from_user.id, 'Список для просмотра:\n' + films_text,
+                               reply_markup=nav.filmsMenu)
     elif message.text == 'Книги':
-        await bot.send_message(message.from_user.id, 'тут будет список книг,\n  '
-                                                     'это тоже нужно дать текстом мне',
-                               reply_markup=nav.improveMenu)
-
+        await bot.send_message(message.from_user.id, 'Список книг, которые могут изменить вышу жизнь:\n' + books_text,
+                               reply_markup=nav.booksMenu)
+    elif message.text == 'Музыка':
+        await bot.send_message(message.from_user.id, 'Ссылки на ресурсы для прослушивания:\n ' + music_text,
+                               reply_markup=nav.musicsMenu)
+    elif message.text == 'Другие материалы':
+        await bot.send_message(message.from_user.id, 'Ссылки на ресурсы: \n' + links_text,
+                               reply_markup=nav.mainMenu)
+    elif message.text == 'Трейдеру':
+        await bot.send_message(message.from_user.id, 'Здесь есть всё, что вам может понадобиться',
+                               reply_markup=nav.traderMenu)
+    elif message.text == 'Арбитражи':
+        await bot.send_message(message.from_user.id, text_arbitrage,
+                               reply_markup=nav.arbitrageMenu)
     else:
         await message.reply('Неизветсная команда')
-
-@dp.message_handler(lambda message: message.text == "1-->111")
-async def without_puree(message: types.Message):
-    await message.reply("111!")
 
 
 # echo - заглушка
--- markup.py
+++ markup.py
@@ -1,17 +1,36 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 btnMain = KeyboardButton('Главное меню')
-# main menu
+# main menu - Приветствие
 btnEdu = KeyboardButton('Учиться')
 btnImprove = KeyboardButton('Стать лучше')
 btnFun = KeyboardButton('Развлечься')
-mainMenu = ReplyKeyboardMarkup(resize_keyboard=True).add(btnEdu, btnImprove, btnFun)
+mainMenu = ReplyKeyboardMarkup(resize_keyboard=True).add(btnEdu, btnImprove, btnFun, btnMain)
 
+# edu menu - из кнопок: вызов главного меню  --   Обучение
+eduMenu = ReplyKeyboardMarkup(resize_keyboard=True).add(btnImprove, btnMain)
 
-#edu menu - из кнопок: вызов главного меню  --   Обучение
-eduMenu = ReplyKeyboardMarkup(resize_keyboard= True).add(btnMain)
+# меню для -- Библиотеки ф м к др мат назад
+btnFilms = KeyboardButton('Фильмы')
+btnMusics = KeyboardButton('Музыка')
+btnBooks = KeyboardButton('Книги')
+btnOtherMaterials = KeyboardButton('Другие материалы')
+btnLibrary = KeyboardButton('Библиотека')
+libraryMenu = ReplyKeyboardMarkup(resize_keyboard=True).add(btnFilms, btnMusics, btnBooks, btnMain)
 
-# меню для   --  Стать лучше
-btnLibrary = KeyboardButton('Книги')
-btnForTrader = KeyboardButton('Трейдеру')
-improveMenu = ReplyKeyboardMarkup(resize_keyboard= True).add(btnLibrary, btnForTrader, btnMain)
+# меню для -- Трейдеру - traderMenu
+btnArbitrage = KeyboardButton('Арбитражи')
+btnLinks = KeyboardButton('Ссылки на ресурсы')
+traderMenu = ReplyKeyboardMarkup(resize_keyboard=True).add(btnArbitrage, btnEdu, btnBooks, btnMain)
+
+# меню для Фильмы
+filmsMenu = ReplyKeyboardMarkup(resize_keyboard=True).add(btnFun, btnMain)
+
+# меню для Музыка
+musicsMenu = ReplyKeyboardMarkup(resize_keyboard=True).add(btnFun, btnMain)
+
+# меню для Книги
+booksMenu = ReplyKeyboardMarkup(resize_keyboard=True).add(btnImprove, btnMain)
+
+# подменю для арбитража
+arbitrageMenu = ReplyKeyboardMarkup(resize_keyboard=True).add(btnImprove, btnMain)
